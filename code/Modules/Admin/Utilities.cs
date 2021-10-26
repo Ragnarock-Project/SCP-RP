b42a55e62d6f9438d418f2781d12ce99d2188a53
Røck <48966234+Rock842@users.noreply.github.com>
2021-10-26 15:53:12 +0200

    Add files via upload

---
+++ code/Modules/Admin/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using Sandbox;
+using SCP;
+
+namespace SCP.Modules.Admin
+{
+	class Utilities : MainPlayer
+	{
+		public bool cloack { get; set; } = false;
+		
+		public Utilities() { }
+
+
+		public void setCloack(bool value)
+		{
+			base.RoleName = "test";
+			this.cloack = value;
+		}
+	}
+}
